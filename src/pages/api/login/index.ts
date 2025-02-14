59680e8566ce86f0c811d79a9579c040eb083626
Steff Van Weereld <steff.vanweereld@aertssen.be>
2025-02-14 15:44:33 +0100

    changed path to dotenv

--- src/pages/api/login/index.ts
+++ src/pages/api/login/index.ts
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import dotenv from 'dotenv'
 
-dotenv.config()
+dotenv.config({path: "../../../../.env"});
 
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true },
